950c7534ce40a4974daed5f910d8666f68307094
Phạm Ngọc Quốc Khánh <pnqkhanh2924@gmail.com>
2026-09-26 15:49:04 +0700

    feat: enable mediacodec

--- scripts/android/ffmpeg.sh
+++ scripts/android/ffmpeg.sh
@@ -355,9 +355,9 @@ if [[ -n ${FFMPEG_KIT_TRIM} ]]; then
   --enable-demuxer=mov,mp3,image2 \
   --enable-muxer=mp4,mp3,image2 \
   --enable-decoder=h264,h264_mediacodec,mpeg4,mjpeg,png,webp,mp3,aac \
-  --enable-encoder=libx264,mpeg4,aac,libmp3lame,mjpeg,png,libwebp \
+  --enable-encoder=libx264,h264_mediacodec,hevc_mediacodec,mpeg4,aac,libmp3lame,mjpeg,png,libwebp \
   --enable-parser=h264,mpeg4video,mjpeg,png,mp3 \
-  --enable-filter=crop,transpose,hflip,vflip,scale,format,null,anull"
+  --enable-filter=crop,transpose,hflip,vflip,scale,format,null,anull,setpts,atempo"
 fi
 
 # ALWAYS BUILD SHARED LIBRARIES
